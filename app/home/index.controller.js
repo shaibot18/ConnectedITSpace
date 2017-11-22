6d12392ac42dec2c901440e48d7f89d06edee7c5
EvsChen <chensy1996@sjtu.edu.cn>
2017-11-23 00:44:44 +0800

    First commit

---
+++ app/home/index.controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Home.IndexController', Controller);
+
+    function Controller(UserService,RoomService) {
+        var vm = this;
+
+        vm.user = null;
+        vm.createRoom = createRoom;
+
+        initController();
+
+        function initController() {
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+        }
+
+        function createRoom() {
+            
+        }
+    }
+})();
