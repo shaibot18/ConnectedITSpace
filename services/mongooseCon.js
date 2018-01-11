60c761958b5a2fbea2b0470180c8a36c1223739d
EvsChen <chensy1996@sjtu.edu.cn>
2018-01-11 13:41:58 +0800

    Make mongoose connection open to both cf environment and local

--- services/mongooseCon.js
+++ services/mongooseCon.js
@@ -1,11 +1,23 @@
 var config = require('config.json');
 var mongoose = require('mongoose');
-var connectionString = config.connectionString;
+var connectionString;
+if (process.env.VCAP_SERVICES){
+    var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+    connectionString = vcap_services["MongoDB-Service"][0].credentials.uri;
+}
+else{
+    connectionString = config.connectionString;
+}
+
 var connectionOptions = {
-    useMongoClient: true
+    // useMongoClient: true
 }
 mongoose.connect(connectionString,connectionOptions,function(err){
     console.error.bind(console, 'connection error:');
 });
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
--- services/roomdata.service.js
+++ services/roomdata.service.js
@@ -4,27 +4,7 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var roomService = require('services/room.service');
 var Q = require('q');
-<<<<<<< HEAD
-var mongoose = require('mongoose');
-var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
-var connectionString;
-if (process.env.VCAP_SERVICES){
-    var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
-    connectionString = vcap_services["MongoDB-Service"][0].credentials.uri;
-}
-else{
-    connectionString = config.connectionString;
-}
-var connectionOptions = {
-    useMongoClient: true
-}
-mongoose.connect(connectionString,connectionOptions);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-=======
 var mongoose = require('services/mongooseCon');
->>>>>>> 5b6835059e8a34eeac0f5c76bf61a7d7aaa257b9
 var Schema = mongoose.Schema;
 var roomdataSchema = new Schema({
     _RoomId: Schema.Types.ObjectId,
