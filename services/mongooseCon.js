40df6e3cc24937cbb3970650f507a354abff52b9
FIXED-TERM CHEN Shenyue (CI/DAV31.5) <csh6sgh@bosch.com>
2018-01-24 09:57:03 +0800

    modify connection option

--- services/mongooseCon.js
+++ services/mongooseCon.js
@@ -1,11 +1,19 @@
 var config = require('config.json');
 var mongoose = require('mongoose');
-var connectionString = config.connectionString;
-var connectionOptions = {
-    useMongoClient: true
+var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+var connectionString;
+if (process.env.VCAP_SERVICES){
+    var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+    connectionString = vcap_services["MongoDB-Service"][0].credentials.uri;
 }
-mongoose.connect(connectionString,connectionOptions,function(err){
-    console.error.bind(console, 'connection error:');
-});
+else{
+    connectionString = config.connectionString;
+}
+var connectionOptions = {
+    // useMongoClient: true
+}
+mongoose.connect(connectionString,connectionOptions);
 mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 module.exports = mongoose;
